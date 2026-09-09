91652a25490e3fb327b2ff851dce0dc244818bdb
dotharer <yeveney1308@gmail.com>
2026-09-09 18:26:05 +0700

    list

--- src/list/S21_list.h
+++ src/list/S21_list.h
@@ -12,13 +12,34 @@ class ListIterator {
  private:
   Node<T>* point;
 
- public:
+  public:
   ListIterator(Node<T>* nd) : point(nd) {}
-  Node<T>* operator++() { return point->Next; }
-  Node<T>* operator--() { return point->Prev; }
+  Node<T>* operator++() { return point = point->Next; }
+  Node<T>* operator++(int) { return point = point->Next; }
+  Node<T>* operator--() { return point = point->Prev; }
   T& operator*() { return point->data; }
   bool operator==(ListIterator& other) { return (point == other.point); }
   bool operator!=(ListIterator& other) { return !(point == other.point); }
+  T& getdata() { return point->data; }
+};
+
+template <typename T>
+class ListConstIterator {
+ private:
+  const Node<T>* point;
+
+ public:
+  ListConstIterator(Node<T>* nd) : point(nd) {}
+  Node<T>* operator++() const { return point = point->Next; }
+  Node<T>* operator++(int) const { return point = point->Next; }
+  Node<T>* operator--() const { return point = point->Prev; }
+  T& operator*() { return point->data; }
+  bool operator==(const ListConstIterator& other) const {
+    return (point == other.point);
+  }
+  bool operator!=(const ListConstIterator& other) const {
+    return !(point == other.point);
+  }
 };
 
 template <typename T>
@@ -26,6 +47,23 @@ class list {
  private:
   Node<T>* front;
   Node<T>* rear;
+
+  void create_node(T& item) {
+    if (rear == nullptr) {
+      rear = new Node<T>;
+      rear->Next = rear->Prev = nullptr;
+      front = rear;
+      rear->data = item;
+      front->data = item;
+    } else {
+      Node<T>* tmp = new Node<T>;
+      tmp->Next = nullptr;
+      tmp->Prev = front;
+      tmp->data = item;
+      front->Next = tmp;
+      front = tmp;
+    }
+  }
 
  public:
   using value_type = T;
@@ -52,31 +90,28 @@ class list {
   list(std::initializer_list<value_type> const& items)
       : front(nullptr), rear(nullptr) {
     for (auto item : items) {
-      if (rear == nullptr) {
-        rear = new Node<T>;
-        rear->Next = rear->Prev = nullptr;
-        front = rear;
-        rear->data = item;
-        front->data = item;
-      } else {
-        Node<T>* tmp = new Node<T>;
-        tmp->Next = nullptr;
-        tmp->Prev = front;
-        tmp->data = item;
-        front->Next = tmp;
-        front = tmp;
-      }
+      create_node(item);
     }
   }
+
+  list(const list& l) : front(nullptr), rear(nullptr) {
+    ListIterator<T> i = l.rear;
+    ListIterator<T> end_i = nullptr;
+    for (; i != end_i; i++) {
+      create_node(i.getdata());
+    }
+  }
+
   iterator begin() { return ListIterator(rear); }
   iterator end() { return ListIterator(front); }
 };
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, list<T> lst) {
-  ListIterator<T> end_iter = lst.end();
-  for (ListIterator<T> i = lst.begin(); i != end_iter; ++i) {
-    os << *i << ' ';
+std::ostream& operator<<(std::ostream& os, list<T>& lst) {
+  ListIterator<T> tmp = lst.begin();
+  ListIterator<T> end_i = nullptr;
+  for (tmp; tmp != end_i; tmp++) {
+    os << *tmp << ' ';
   }
   os << std::endl;
   return os;
--- src/list/test_list.cc
+++ src/list/test_list.cc
@@ -5,4 +5,6 @@
 int main() {
   S21::list<int> lst1({1, 2, 3, 4});
   std::cout << lst1;
+  S21::list<int> copy_lst(lst1);
+  std::cout << copy_lst;
 }
