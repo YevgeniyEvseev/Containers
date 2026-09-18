6572e297fcd70c1e40b8f01a845a829254e7cb99
dotharer <yeveney1308@gmail.com>
2026-09-18 18:29:13 +0700

    list

--- src/list/S21_list.h
+++ src/list/S21_list.h
@@ -1,3 +1,5 @@
+#define MAX_SIZE 9223372036854775807
+
 namespace S21 {
 
 template <typename T>
@@ -12,15 +14,16 @@ class ListIterator {
  private:
   Node<T>* point;
 
-  public:
+ public:
   ListIterator(Node<T>* nd) : point(nd) {}
   Node<T>* operator++() { return point = point->Next; }
   Node<T>* operator++(int) { return point = point->Next; }
   Node<T>* operator--() { return point = point->Prev; }
   T& operator*() { return point->data; }
   bool operator==(ListIterator& other) { return (point == other.point); }
-  bool operator!=(ListIterator& other) { return !(point == other.point); }
+  bool operator!=(ListIterator& other) { return point != other.point; }
   T& getdata() { return point->data; }
+  Node<T>* get_point() { return point; }
 };
 
 template <typename T>
@@ -45,24 +48,31 @@ class ListConstIterator {
 template <typename T>
 class list {
  private:
-  Node<T>* front;
+  Node<T>* front_n;
   Node<T>* rear;
 
   void create_node(T& item) {
     if (rear == nullptr) {
       rear = new Node<T>;
       rear->Next = rear->Prev = nullptr;
-      front = rear;
+      front_n = rear;
       rear->data = item;
-      front->data = item;
+      front_n->data = item;
     } else {
       Node<T>* tmp = new Node<T>;
       tmp->Next = nullptr;
-      tmp->Prev = front;
+      tmp->Prev = front_n;
       tmp->data = item;
-      front->Next = tmp;
-      front = tmp;
+      front_n->Next = tmp;
+      front_n = tmp;
     }
+  }
+
+  void create_end_node(size_t n) {
+    Node<T>* end_l = new Node<T>;
+    front_n->Next = end_l;
+    end_l->Prev = front_n;
+    end_l->data = n++;
   }
 
  public:
@@ -72,45 +82,127 @@ class list {
   using size_type = size_t;
   typedef ListIterator<T> iterator;
 
-  list() : front(nullptr), rear(nullptr) {}
+  list() : front_n(nullptr), rear(nullptr) {}
   list(size_type n) {
     if (n > 0) {
       rear = new Node<T>;
       rear->Next = rear->Prev = nullptr;
-      front = rear;
+      front_n = rear;
       for (int i = 1; i < n; i++) {
-        Node<T>* tmp = front;
-        front = new Node<T>;
-        front->Next = nullptr;
-        front->Prev = tmp;
-        tmp->Next = front;
+        Node<T>* tmp = front_n;
+        front_n = new Node<T>;
+        front_n->Next = nullptr;
+        front_n->Prev = tmp;
+        tmp->Next = front_n;
       }
+      create_end_node(n);
     }
   }
   list(std::initializer_list<value_type> const& items)
-      : front(nullptr), rear(nullptr) {
+      : front_n(nullptr), rear(nullptr) {
+    size_t count = 0;
     for (auto item : items) {
       create_node(item);
+      count++;
     }
+    create_end_node(count);
   }
 
-  list(const list& l) : front(nullptr), rear(nullptr) {
+  list(const list& l) : front_n(nullptr), rear(nullptr) {
     ListIterator<T> i = l.rear;
     ListIterator<T> end_i = nullptr;
+    size_t count = 0;
     for (; i != end_i; i++) {
       create_node(i.getdata());
+      count++;
     }
+    create_end_node(count);
   }
 
+  list(list&& l) {
+    front_n = l.front_n;
+    rear = l.rear;
+    l.rear = nullptr;
+    l.front_n = nullptr;
+  }
+
+  ~list() { clear(); }
+
+  list<T>& operator=(list&& l) {
+    rear = l.rear;
+    front_n = l.front_n;
+    l.rear = nullptr;
+    l.front_n = nullptr;
+    return *this;
+  }
+
+  const_reference front() { return front_n->data; }
+  const_reference back() { return rear->data; }
+
   iterator begin() { return ListIterator(rear); }
-  iterator end() { return ListIterator(front); }
+  iterator end() { return front_n->Next; }
+
+  bool empty() { return (rear == nullptr && front = nullptr); }
+
+  size_type size() {
+    size_type i = 0;
+    Node<T>* tmp = rear;
+    while (tmp != nullptr) {
+      i++;
+      tmp = tmp->Next;
+    }
+    return i;
+  }
+
+  size_type max_size() { return MAX_SIZE / sizeof(T); }
+
+  void clear() {
+    bool loop = true;
+    Node<T>* tmp = rear;
+    Node<T>* i = tmp;
+    while (i != nullptr) {
+      i = i->Next;
+      delete tmp;
+      tmp = i;
+    }
+    rear = nullptr;
+    front_n = nullptr;
+  }
+
+  iterator insert(iterator pos, const_reference value) {
+    Node<T>* tmp = new Node<T>;
+    Node<T>* pos_node = pos.get_point();
+    tmp->data = value;
+    tmp->Next = pos_node;
+    tmp->Prev = pos_node->Prev;
+    if (pos_node->Prev != nullptr) {
+      pos_node->Prev->Next = tmp;
+    } else {
+      rear = tmp;
+    }
+    if (pos_node->Next != nullptr) {
+      pos_node->Next->Prev = tmp;
+    } else {
+      front_n = tmp;
+    }
+    return tmp;
+  }
+
+  void erase(iterator pos) {
+    Node<T>* tmp = pos.get_point();
+    tmp->Next->Prev = tmp->Prev;
+    tmp->Prev->Next = tmp->Next;
+    delete tmp;
+  }
+
+  void push_back(const_reference value) { insert(end(), value); }
 };
 
 template <typename T>
 std::ostream& operator<<(std::ostream& os, list<T>& lst) {
   ListIterator<T> tmp = lst.begin();
-  ListIterator<T> end_i = nullptr;
-  for (tmp; tmp != end_i; tmp++) {
+  ListIterator<T> end_i = lst.end();
+  for (tmp = lst.begin(); tmp != end_i; tmp++) {
     os << *tmp << ' ';
   }
   os << std::endl;
--- src/list/check.cc
+++ src/list/check.cc
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <list>
 
 #include "S21_list.h"
 
-int main() {
-  S21::list<int> lst1({1, 2, 3, 4});
-  std::cout << lst1;
-  S21::list<int> copy_lst(lst1);
+void func1(S21::list<int> t) {
+  S21::list<int> copy_lst = t;
   std::cout << copy_lst;
+}
+
+int main() {
+  using std::cout;
+  using std::endl;
+  S21::list<int> lst1({1, 2, 3, 4, 5});
+  std::cout << lst1;
+  for (auto i : lst1) cout << i;
+
+  func1(lst1);
+  std::cout << lst1;
+  std::cout << "front" << lst1.front() << endl;
+  std::list<int> lst_std({1, 2, 3, 4, 5});
+  lst_std.insert(lst_std.end(), 4);
+  for (auto i : lst_std) std::cout << i << ' ';
+  cout << "end list std " << *(lst_std.end()) << endl;
+  S21::ListIterator<int> pos_s = lst1.begin();
+  cout << "pos_s " << *pos_s;
+  lst1.insert(lst1.end(), 4);
+  std::cout << "list after insert " << lst1;
+  lst1.push_back(6);
+  cout << "push back s21" << lst1;
+  lst_std.push_back(6);
+  cout << "push back std";
+  for (auto i : lst_std) std::cout << i << ' ';
+  cout << endl;
 }
